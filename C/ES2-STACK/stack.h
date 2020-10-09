8fd2864bd5e21e65548a109ccb8fb4019ab9eb89
Nicolò Maffi <71829227+nikmaffi@users.noreply.github.com>
2020-10-09 21:58:40 +0200

    Add files via upload

---
+++ C/ES2-STACK/stack.h
@@ -0,0 +1,54 @@
+/*
+ * stack.h
+ * 
+ * Implementazione di uno stack in C
+ *
+ * Autore:   Nicolò Maffi
+ * Data:     16/05/2020
+ * Versione: 1.2
+*/
+
+#include <stdio.h> //Per size_t
+#include <stdlib.h> //Per calloc
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct node_t //Tipo node -> rappresenta una cella dello stack
+{
+    int value; //Valore del nodo
+    struct node_t *prev; //Puntatore del nodo precedente
+}node;
+
+typedef node* stack; //Tipo stack -> coincide con il puntatore alla cima dello stack
+
+//Funzionne che inizializza lo stack
+//VALORE DI RITORNO: stack inizializzato
+stack initstack();
+
+//Funzione di push -> inserimento di un dato nello stack
+//PARAMETRI: s = puntatore allo stack su cui si vuole effettuare la push
+//           value = valore che si vuole far assumere al nodo
+//VALORE DI RITORNO: 1 se loperazione è riuscita, 0 altrimenti
+short push(stack *s, int value);
+
+//Funzione di pop -> rimozione di un dato dallo stack
+//PARAMETRI: s = puntatore allo stack su cui si vuole effettuare la push
+//           value = valore del nodo che ritornerà la push
+//VALORE DI RITORNO: 1 se loperazione è riuscita, 0 altrimenti
+short pop(stack *s, int *value);
+
+//Funzione stackdim -> funzione che calcola la dimensione dello stack
+//PARAMETRI: s = puntatore allo stack su cui si vuole effettuare il calcolo della dimensione
+//VALORE DI RITORNO: dimensione dello stack se loperazione è riuscita, 0 altrimenti
+size_t stackdim(stack *s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_STACK_H_
